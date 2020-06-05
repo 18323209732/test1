73b7a93ec798790dbb423029d32f14aecf5ac0a0
宋宇 <songyu@gouuse.cn>
2020-06-05 16:30:34 +0800

    sy

---
+++ Door/content/Public.py
@@ -0,0 +1,16 @@
+
+# coding=utf-8
+import requests
+from random import randint,choice
+import random,os,re
+import json
+import requests
+import time
+from random import randint
+from datetime import date,timedelta
+from Common.ReadYaml import ReadPublic
+
+def Public_path():
+    path = os.path.dirname(os.path.abspath('.')) + '/content/Public.yaml'
+    print(path)
+    return path
