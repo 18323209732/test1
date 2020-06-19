ac5ee8872ba4bb0cac04726438c68ce2e5d74da8
陈建波 <chenjianbo@300.cn>
2020-06-19 09:01:33 +0800

    提交用例22222

--- Door/news/Public.py
+++ Door/news/Public.py
@@ -8,11 +8,13 @@ import requests
 import time
 from random import randint
 from datetime import date,timedelta
+import urllib3
+from Common.CusMethod import random_str
 from Common.GetToken import Get_Cookies
 from Common.ReadWriteIni import ReadWrite
 from Common.ReadYaml import ReadPublic, ConfigYaml
 projectName = ConfigYaml("projectName").base_config
-url = ConfigYaml(projectName).base_url
+
 
 
 class Public_Data:
@@ -22,6 +24,7 @@ class Public_Data:
         self.type = ConfigYaml('type_key').base_config
         self.form_type = ConfigYaml('form_type').base_config
         self.cookies_key = ConfigYaml('cookies').base_config
+        self.json_type = ConfigYaml('json_type').base_config
         self.cookies_value = ReadWrite(sign='session', option='cookies').read_ini_cookies()
         self.headers = {self.type: self.form_type}
         self.headers.update({self.cookies_key: self.cookies_value})
@@ -35,6 +38,8 @@ class Public_Data:
         获取新闻资讯列表数据
         :return:
         '''
+
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         self.public_data = ReadPublic(catalog='news', key="get_news")
         url = self.public_data.public_value("url") + f"?{self.tenant_key}={self.tenant_value}"
         url = self.url + url
@@ -43,16 +48,22 @@ class Public_Data:
         result = r.json()
         if result.get('status') == 200:
             if swich:
-                while True:
-                    for value in result.get('data').get('pres'):
-                        yield value.get('title')
+                if not result.get('data').get('pres'):
+                    self.add_news()
+                try:
+                    value = choice(result.get('data').get('pres'))
+                    yield value.get('title')
+                except StopIteration:
+                    pass
 
             else:
-                while True:
-                    for value in result.get('data').get('pres'):
-                        yield value.get('id')
-
-
+                if not result.get('data').get('pres'):
+                    self.add_news()
+                try:
+                    value = choice(result.get('data').get('pres'))
+                    yield value.get('id')
+                except StopIteration:
+                    pass
 
     @property
     def get_news_class(self):
@@ -60,6 +71,8 @@ class Public_Data:
         获取新闻资讯分类数据
         :return:
         '''
+
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         self.public_data = ReadPublic(catalog='news', key="get_news_class")
         url = self.public_data.public_value("url") + f"?{self.tenant_key}={self.tenant_value}"
         url = self.url + url
@@ -67,8 +80,72 @@ class Public_Data:
         r = requests.post(url, headers=self.headers, json=data, stream=True, verify=False)
         result = r.json()
         if result.get('status') == 200:
-            while True:
-                for value in result.get('data').get('data'):
-                    yield value.get('id')
+            try:
+                value = choice(result.get('data').get('data'))
+                yield value.get('id')
+            except StopIteration:
+                pass
 
+    def add_news(self):
+        '''
+        获取新闻资讯分类数据
+        :return:
+        '''
+
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        self.public_data = ReadPublic(catalog='news', key="add_news")
+        self.headers[self.type] = self.json_type
+        url = self.public_data.public_value("url") + f"?{self.tenant_key}={self.tenant_value}"
+        url = self.url + url
+        data = self.public_data.public_value("bar")
+
+        data['title'] = random_str("自动化新增新闻资讯")
+        data['content'] = random_str("<p>自动化新增新闻资讯内容....</p>\n")
+        data['infotype'] = str(next(self.get_news_class))
+        r = requests.post(url, headers=self.headers, json=data, stream=True, verify=False)
+
+    @property
+    def get_pictures(self):
+        '''
+        获取新闻资讯分类数据
+        :return:
+        '''
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        self.public_data = ReadPublic(catalog='news', key="get_picture")
+        url = self.public_data.public_value("url") + f"?{self.tenant_key}={self.tenant_value}"
+        url = self.url + url
+        data = self.public_data.public_value("bar")
+        r = requests.get(url, headers=self.headers, data=data, stream=True, verify=False)
+        result = r.json()
+        if result.get('data'):
+            try:
+                value = choice(result.get('data').get('list'))
+                yield value.get('imgUrl')
+            except StopIteration:
+                pass
+
+    @property
+    def get_application(self):
+        '''
+        获取新闻资讯分类数据
+        :return:
+        '''
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        self.public_data = ReadPublic(catalog='news', key="get_application")
+        url = self.public_data.public_value("url") + f"?{self.tenant_key}={self.tenant_value}"
+
+        url = self.url + url
+        data = self.public_data.public_value("bar")
+        r = requests.post(url, headers=self.headers, data=data, stream=True, verify=False)
+        result = r.json()
+        if result.get("status"):
+            try:
+                value = choice(result.get('data').get("relatedContentList"))
+                yield value.get('id')
+            except StopIteration:
+                pass
+
+if __name__ == "__main__":
+    # print(next(Public_Data().get_application))
+    pass
 
