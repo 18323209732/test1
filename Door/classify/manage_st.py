55070a64781dc1c198ded0c5ffb92f4247609510
方坤 <fangkun@gouuse.cn>
2020-06-28 09:41:20 +0800

    拉代码123

---
+++ Door/classify/manage_st.py
@@ -0,0 +1,183 @@
+# coding=utf-8
+import unittest
+import traceback
+import requests
+from Common.FontColor import outcome
+from Common.MyUnit import MyTest
+from Common.ReadYaml import ConfigYaml
+from Common.DataHandle import ReRun
+import urllib3
+import time
+from Door.classify.Public import Classify
+
+
+class manage_classify(MyTest):
+
+    condition = True
+    type_condition = True
+    # 介绍内容-分类管理
+    # @unittest.skipIf(condition, "暂时跳过")
+    @ReRun(MyTest.setUp)
+    def test_classify_manage(self):
+        # 分类管理页面
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        try:
+            if self.type_condition:
+                self.headers[self.type] = self.form_type
+                
+            url = ConfigYaml(self.projectName).base_url + self.url + 'viewType=1&node=root'
+            r = requests.get(url, headers=self.headers, json=self.data, stream=True, verify=False)
+            self.result = r.json()
+
+            self.time = r.elapsed.total_seconds()
+        except:
+            self.singular = str(traceback.format_exc())
+            outcome('red', self.singular)
+            return self.singular
+        
+    # @unittest.skipIf(condition, "暂时跳过")
+    @ReRun(MyTest.setUp)
+    def test_add_classify(self):
+        # 添加分类
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        self.type_condition = False
+        try:
+            if self.type_condition:
+                self.headers[self.type] = self.form_type
+                
+            url = ConfigYaml(self.projectName).base_url + self.url
+            self.data['introductionCategory']['name'] = '接口分类{}'.format(time.time())
+            r = requests.post(url, headers=self.headers, json=self.data, stream=True, verify=False)
+            self.result = r.json()
+
+            self.time = r.elapsed.total_seconds()
+        except:
+            self.singular = str(traceback.format_exc())
+            outcome('red', self.singular)
+            return self.singular
+        
+    # @unittest.skipIf(condition, "暂时跳过")
+    @ReRun(MyTest.setUp)
+    def test_edit_classify(self):
+        # 编辑分类
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        try:
+            if self.type_condition:
+                self.headers[self.type] = self.form_type
+            id = Classify().add_classify() #添加分类id
+            url = ConfigYaml(self.projectName).base_url + self.url
+            self.data['introductionCategory']['name'] = '编辑分类{}'.format(time.time())
+            self.data['introductionCategory']['id'] = id
+            r = requests.post(url, headers=self.headers, json=self.data, stream=True, verify=False)
+            self.result = r.json()
+
+            self.time = r.elapsed.total_seconds()
+        except:
+            self.singular = str(traceback.format_exc())
+            outcome('red', self.singular)
+            return self.singular
+        
+    # @unittest.skipIf(condition, "暂时跳过")
+    @ReRun(MyTest.setUp)
+    def test_see_classify(self):
+        # 查看分类
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        try:
+            if self.type_condition:
+                self.headers[self.type] = self.form_type
+
+            id = Classify().add_classify()  # 添加分类id
+            url = ConfigYaml(self.projectName).base_url + self.url + '&viewType=1&categoryId={}&isdisplay=&title=&ismobile=&endDate=&ec_crd=15&ec_p=1&beginDate=&category={}'.format(id,id)
+            print(url)
+            print(self.headers)
+            r = requests.get(url, headers=self.headers, json=self.data, stream=True, verify=False)
+            self.result = r.json()
+            print(self.result)
+
+            self.time = r.elapsed.total_seconds()
+        except:
+            self.singular = str(traceback.format_exc())
+            outcome('red', self.singular)
+            return self.singular
+        
+    # @unittest.skipIf(condition, "暂时跳过")
+    @ReRun(MyTest.setUp)
+    def test_transfer_classify(self):
+        # 转移分类
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        try:
+            if self.type_condition:
+                self.headers[self.type] = self.form_type
+
+            id = Classify().add_classify()  # 添加分类id
+            pid = Classify().add_classify()  # 添加分类id
+            url = ConfigYaml(self.projectName).base_url + self.url + '&id={}&pid={}'.format(id,pid)
+            r = requests.get(url, headers=self.headers, json=self.data, stream=True, verify=False)
+            self.result = r.json()
+
+            self.time = r.elapsed.total_seconds()
+        except:
+            self.singular = str(traceback.format_exc())
+            outcome('red', self.singular)
+            return self.singular
+        
+    # @unittest.skipIf(condition, "暂时跳过")
+    @ReRun(MyTest.setUp)
+    def test_hide_classify(self):
+        # 隐藏电脑版
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        try:
+            if self.type_condition:
+                self.headers[self.type] = self.form_type
+
+            id = Classify().add_classify()  # 添加分类id
+            url = ConfigYaml(self.projectName).base_url + self.url
+            self.data['id'] = id
+            r = requests.post(url, headers=self.headers, data=self.data, stream=True, verify=False)
+            self.result = r.json()
+
+            self.time = r.elapsed.total_seconds()
+        except:
+            self.singular = str(traceback.format_exc())
+            outcome('red', self.singular)
+            return self.singular
+        
+    # @unittest.skipIf(condition, "暂时跳过")
+    @ReRun(MyTest.setUp)
+    def test_display_classify(self):
+        # 显示电脑版
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        try:
+            if self.type_condition:
+                self.headers[self.type] = self.form_type
+            id = Classify().add_hide_classify()
+            url = ConfigYaml(self.projectName).base_url + self.url
+            self.data['id'] = id
+            r = requests.post(url, headers=self.headers, data=self.data, stream=True, verify=False)
+            self.result = r.json()
+
+            self.time = r.elapsed.total_seconds()
+        except:
+            self.singular = str(traceback.format_exc())
+            outcome('red', self.singular)
+            return self.singular
+        
+    # @unittest.skipIf(condition, "暂时跳过")
+    @ReRun(MyTest.setUp)
+    def test_del_classify(self):
+        # 删除分类
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        try:
+            if self.type_condition:
+                self.headers[self.type] = self.form_type
+            id = Classify().add_hide_classify()
+            url = ConfigYaml(self.projectName).base_url + self.url + '&id={}'.format(id)
+            r = requests.get(url, headers=self.headers, json=self.data, stream=True, verify=False)
+            self.result = r.json()
+
+            self.time = r.elapsed.total_seconds()
+        except:
+            self.singular = str(traceback.format_exc())
+            outcome('red', self.singular)
+            return self.singular
+        
