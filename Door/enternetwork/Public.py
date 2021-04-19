73eaae3b6ba11ab0270e9919360f64c7e64d4229
tanghui28 <425475513@qq.com>
2021-04-19 16:51:02 +0800

    test

--- Door/enternetwork/Public.py
+++ Door/enternetwork/Public.py
@@ -55,17 +55,17 @@ class Public_Data:
         img_id = value.get("id")
         img_url = value.get("imgUrl")
         name = value.get("name")
-        # data['images'][0]["imgId"] = img_id
-        # data['images'][0]["url"] = img_url
-        # data['images'][0]["thumbId"] = img_id
-        # data['images'][0]["imgUrl"] = img_url
-        # data['images'][0]["imageName"] = name
-        # data['name'] = random_str("自动化新增企业网点")
-        # data['description'] = random_str("<p>自动化新增企业网点详情数据....</p>\n")
-        # data['keywords'] = random_str("自动化新增企业网点关键词")
-        # data['summary'] = random_str("自动化新增企业网点描述")
-        # print(data)
-        data = f"""name={random_str('自动化新增企业网点')}&categoryId=2&locaJson=[]&cityName=成都市&provinceName=四川省&districtName=锦江区&address=天府半岛&lng=104.071161&lat=30.482808&zipCode=&website=&wechat=&statu=1&syncStatu=&images=[{'imgId':{img_id},'url': {img_url},'thumbId':{img_id},'imgUrl': '{img_url}','imageName'':'{name}','iscover'':1,'sequence'':'','height'':'','wide'':''}]&description=random_str('<p>自动化新增企业网点详情数据....</p>\n')&mobileDesc=&keywords=&cardId=&mobileStatu=1&seoAuto=true&viewCount=568&seoTitle=[{'id':'name','name':'网点名称'},{'id':'siteName','name':'网站名称'}]&seoTitleSign=_&seoKeywords=[{'id':'keyword','name':'网点关键词'},{'id':'name','name':'网点关键词'},{'id':'siteName','name':'网点名称'}]&seoKeywordsSign=,&seoDescription=[{'id':'name','name':'网点名称'},{'id':'siteName','name':'网点名称'},{'id':'summary','name':'网点概要'}]&seoDescriptionSign=-&seoAddDescription=undefined&logoUrl={img_url}&logoId={img_id}&fullAddress=成都市锦江区&summary=&locationArray=[]&citys=['四川省','成都市'','锦江区']&relationListJson=[{'appId':11,'appName'':'企业网点','contentList'':[]}]&"""
+        data['images'][0]["imgId"] = img_id
+        data['images'][0]["url"] = img_url
+        data['images'][0]["thumbId"] = img_id
+        data['images'][0]["imgUrl"] = img_url
+        data['images'][0]["imageName"] = name
+        data['name'] = random_str("自动化新增企业网点")
+        data['description'] = random_str("<p>自动化新增企业网点详情数据....</p>\n")
+        data['keywords'] = random_str("自动化新增企业网点关键词")
+        data['summary'] = random_str("自动化新增企业网点描述")
+        print(data)
+        # data = f"""name={random_str('自动化新增企业网点')}&categoryId=2&locaJson=[]&cityName=成都市&provinceName=四川省&districtName=锦江区&address=天府半岛&lng=104&lat=30&zipCode=&website=&wechat=&statu=1&syncStatu=&images=[{'imgId':{img_id},'url': {img_url},'thumbId':{img_id},'imgUrl': '{img_url}','imageName'':'{name}','iscover'':1,'sequence'':'','height'':'','wide'':''}]&description=random_str('<p>自动化新增企业网点详情数据....</p>\n')&mobileDesc=&keywords=&cardId=&mobileStatu=1&seoAuto=true&viewCount=568&seoTitle=[{'id':'name','name':'网点名称'},{'id':'siteName','name':'网站名称'}]&seoTitleSign=_&seoKeywords=[{'id':'keyword','name':'网点关键词'},{'id':'name','name':'网点关键词'},{'id':'siteName','name':'网点名称'}]&seoKeywordsSign=,&seoDescription=[{'id':'name','name':'网点名称'},{'id':'siteName','name':'网点名称'},{'id':'summary','name':'网点概要'}]&seoDescriptionSign=-&seoAddDescription=undefined&logoUrl={img_url}&logoId={img_id}&fullAddress=成都市锦江区&summary=&locationArray=[]&citys=['四川省','成都市'','锦江区']&relationListJson=[{'appId':11,'appName'':'企业网点','contentList'':[]}]"""
 
         r = requests.post(url, headers=self.headers, data=data, stream=True, verify=False)
         print(r.json())
